e9989c4ade54bd9b653e22515358295a6afe335d
molarmanful <molarmanful@users.noreply.github.com>
2014-11-16 14:41:06 -0800

    Update main.js

--- main.js
+++ main.js
@@ -68,8 +68,8 @@ $(document).ready(function(){
 	$('#pop').css('display', 'inline-block');
   });
   $('#two').click(function(){
-  	cube.edges.setOpacity(0, 10);
-	cube.centers.setOpacity(0, 10);
+  	cube.edges.setOpacity(0, -1);
+	cube.centers.setOpacity(0, -1);
 	cube.corners.setRadius(-60, 10);
 	$('.cube').css('height', '150%');
 	$('.cube').css('width', '150%');
@@ -79,8 +79,8 @@ $(document).ready(function(){
 	$('#reassemble').fadeOut('fast');
   });
   $('#three').click(function(){
-  	cube.edges.setOpacity(1, 10);
-  	cube.centers.setOpacity(1, 10);
+  	cube.edges.setOpacity(1, 1);
+  	cube.centers.setOpacity(1, 1);
   	cube.corners.setRadius(0, 10);
   	$('.cube').css('height', '100%');
   	$('.cube').css('width', '100%');
